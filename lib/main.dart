267b3ab9bc9c59dfc2c4e2c6a2546615fde4b00d
tarik <mdtarikbinaziz@gmail.com>
2025-01-25 16:29:49 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,220 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// // by stack
+// class BottomNavBarElasticLine extends StatefulWidget {
+//   const BottomNavBarElasticLine({super.key});
+
+//   @override
+//   _BottomNavBarElasticLineState createState() =>
+//       _BottomNavBarElasticLineState();
+// }
+
+// class _BottomNavBarElasticLineState extends State<BottomNavBarElasticLine>
+//     with SingleTickerProviderStateMixin {
+//   int _selectedIndex = 0;
+//   late AnimationController _controller;
+//   late Animation<double> _lineWidthAnimation;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = AnimationController(
+//       vsync: this,
+//       duration: const Duration(milliseconds: 300),
+//     );
+//     _lineWidthAnimation = Tween<double>(begin: 0, end: 60).animate(
+//       CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic),
+//     );
+//   }
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//     _controller.forward(from: 0);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final itemCount = 4;
+//     final itemWidth = MediaQuery.of(context).size.width / itemCount;
+
+//     return Scaffold(
+//       body: Center(
+//         child: Text('Selected Index: $_selectedIndex'),
+//       ),
+//       bottomNavigationBar: Stack(
+//         alignment: Alignment.bottomCenter,
+//         children: [
+//           // Navigation items
+//           Container(
+//             height: 70,
+//             color: Colors.white,
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: List.generate(itemCount, (index) {
+//                 final isSelected = _selectedIndex == index;
+//                 return GestureDetector(
+//                   onTap: () => _onItemTapped(index),
+//                   child: Column(
+//                     mainAxisSize: MainAxisSize.min,
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       Icon(
+//                         Icons.circle,
+//                         color: isSelected ? Colors.blue : Colors.grey,
+//                       ),
+//                       const SizedBox(height: 4),
+//                       Text(
+//                         'Item $index',
+//                         style: TextStyle(
+//                           color: isSelected ? Colors.blue : Colors.grey,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 );
+//               }),
+//             ),
+//           ),
+//           // Animated line
+//           AnimatedBuilder(
+//             animation: _lineWidthAnimation,
+//             builder: (context, child) {
+//               return Positioned(
+//                 bottom: 0,
+//                 left: itemWidth * _selectedIndex +
+//                     (itemWidth / 2) -
+//                     (_lineWidthAnimation.value / 2),
+//                 child: Container(
+//                   height: 4,
+//                   width: _lineWidthAnimation.value,
+//                   decoration: BoxDecoration(
+//                     color: Colors.blue,
+//                     borderRadius: BorderRadius.circular(2),
+//                   ),
+//                 ),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+// }
+
+// // by  animated container
+// class BottomNavBarElasticLine extends StatefulWidget {
+//   const BottomNavBarElasticLine({super.key});
+
+//   @override
+//   _BottomNavBarElasticLineState createState() =>
+//       _BottomNavBarElasticLineState();
+// }
+
+// class _BottomNavBarElasticLineState extends State<BottomNavBarElasticLine>
+//     with SingleTickerProviderStateMixin {
+//   int _selectedIndex = 0;
+//   late AnimationController _controller;
+//   late Animation<double> _lineWidthAnimation;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = AnimationController(
+//       vsync: this,
+//       duration: const Duration(milliseconds: 300),
+//     );
+//     _lineWidthAnimation = Tween<double>(begin: 0, end: 40).animate(
+//       CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic),
+//     );
+//   }
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//     _controller.forward(from: 0);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final itemCount = 4;
+
+//     return Scaffold(
+//       body: Center(
+//         child: Text('Selected Index: $_selectedIndex'),
+//       ),
+//       bottomNavigationBar: Container(
+//         height: 70,
+//         color: Colors.white,
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             // Navigation items
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: List.generate(itemCount, (index) {
+//                 final isSelected = _selectedIndex == index;
+//                 return GestureDetector(
+//                   onTap: () => _onItemTapped(index),
+//                   child: Column(
+//                     mainAxisSize: MainAxisSize.min,
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       Icon(
+//                         Icons.circle,
+//                         color: isSelected ? Colors.blue : Colors.grey,
+//                       ),
+//                       const SizedBox(height: 4),
+//                       Text(
+//                         'Item $index',
+//                         style: TextStyle(
+//                           color: isSelected ? Colors.blue : Colors.grey,
+//                         ),
+//                       ),
+//                       // Animated Line under the item title
+//                       if (isSelected)
+//                         AnimatedBuilder(
+//                           animation: _lineWidthAnimation,
+//                           builder: (context, child) {
+//                             return Padding(
+//                               padding: const EdgeInsets.only(top: 8),
+//                               child: Container(
+//                                 height: 2,
+//                                 width: _lineWidthAnimation.value,
+//                                 color: Colors.blue,
+//                               ),
+//                             );
+//                           },
+//                         ),
+//                     ],
+//                   ),
+//                 );
+//               }),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+// }
+
+
+// without stack
 class BottomNavBarElasticLine extends StatefulWidget {
   const BottomNavBarElasticLine({super.key});
 
@@ -41,7 +255,7 @@ class _BottomNavBarElasticLineState extends State<BottomNavBarElasticLine>
       vsync: this,
       duration: const Duration(milliseconds: 300),
     );
-    _lineWidthAnimation = Tween<double>(begin: 0, end: 60).animate(
+    _lineWidthAnimation = Tween<double>(begin: 0, end: 40).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic),
     );
   }
@@ -62,14 +276,15 @@ class _BottomNavBarElasticLineState extends State<BottomNavBarElasticLine>
       body: Center(
         child: Text('Selected Index: $_selectedIndex'),
       ),
-      bottomNavigationBar: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          // Navigation items
-          Container(
-            height: 70,
-            color: Colors.white,
-            child: Row(
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(8),
+        height: 70,
+        color: Colors.white,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Navigation items in a row
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: List.generate(itemCount, (index) {
                 final isSelected = _selectedIndex == index;
@@ -90,33 +305,30 @@ class _BottomNavBarElasticLineState extends State<BottomNavBarElasticLine>
                           color: isSelected ? Colors.blue : Colors.grey,
                         ),
                       ),
+                      const SizedBox(height: 4),
+                      // Animated line container under the selected item
+
+                      AnimatedBuilder(
+                        animation: _lineWidthAnimation,
+                        builder: (context, child) {
+                          return Transform.scale(
+                            scaleX: 1.0,
+                            child: Container(
+                              height: 2,
+                              width: _lineWidthAnimation.value,
+                              color:
+                                  isSelected ? Colors.blue : Colors.transparent,
+                            ),
+                          );
+                        },
+                      ),
                     ],
                   ),
                 );
               }),
             ),
-          ),
-          // Animated line
-          AnimatedBuilder(
-            animation: _lineWidthAnimation,
-            builder: (context, child) {
-              return Positioned(
-                bottom: 0,
-                left: itemWidth * _selectedIndex +
-                    (itemWidth / 2) -
-                    (_lineWidthAnimation.value / 2),
-                child: Container(
-                  height: 4,
-                  width: _lineWidthAnimation.value,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(2),
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
